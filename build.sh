5c350b8d8813fbda66e12f38bade087c3c8ca227
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-04 17:44:47 -0300

    Add build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,26 @@
 # TODO: Get the .pkg.tar.zst from calpkgs
 
 buildDatabase() {
-    repo-add -s -n -R -k "199980CE93F18E62" calrepo.db.tar.gz *.pkg.tar.zst
-    rm -rf *.old*
+    cd db || { echo "Couldn't cd into 'db'." 1>&2 && exit 1; }
+
+    while read -r pkg; do
+        repo-add -s -n -R -k "199980CE93F18E62" calrepo.db.tar.gz "$pkg"
+    done < "$1"
+
+    cd "$lastFolder" || { echo "Couldn't cd into '$lastFolder'." 1>&2 && exit 1; }
 }
 
 runScript() {
-    lastFolder=$(pwd -P)
-    cd x86_64 || { echo "Couldn't cd into 'x86_64'." 1>&2 && exit 1; }
+#     lastFolder=$(pwd -P)
 
-    buildDatabase
+#     local IFS=
+#     while read -r -d '' arch; do
+#         cd "$arch" || { echo "Couldn't cd into 'x86_64'." 1>&2 && exit 1; }
+#         buildDatabase
+#         cd "$lastFolder" || { echo "Couldn't cd into '$lastFolder'." 1>&2 && exit 1; }
+#    done < <(find -H "$PWD" -mindepth 1 -type f -regex '.*.pkg.tar.zst' -print0)
 
-    cd "$lastFolder" || { echo "Couldn't cd into '$lastFolder'." 1>&2 && exit 1; }
+    buildDatabase <(find -H "$PWD" -mindepth 1 -type f -regex '.*.pkg.tar.zst')
 }
 
 runScript
