3b93acb95e47c219e34b309fb731c5e300aac3eb
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-06 17:56:45 -0300

    Run shellcheck

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 getAnyPackages() {
-    pkgs=($(find -H "$PWD/any" -mindepth 1 -type f -regex '.*.pkg.tar.zst.*'))
+    mapfile -t pkgs <(find -H "$PWD/any" -mindepth 1 -type f -regex '.*.pkg.tar.zst.*')
     for arch in "x86_64" "i686" "aarch64"; do
         cd "$arch" || { echo "Couldn't cd into '$arch'." 1>&2 && exit 1; }
         for pkg in "${pkgs[@]}"; do
             if [ "$dryRunFlag" = false ]; then
-                ln -sf "$pkg" "$(basename $pkg)"
+                ln -sf "$pkg" "$(basename "$pkg")"
             else
-                echo "ln -sf $pkg $(basename $pkg)"
+                echo "ln -sf $pkg $(basename "$pkg")"
             fi
         done
         cd .. || { echo "Couldn't cd into the parent folder." 1>&2 && exit 1; }
@@ -23,7 +23,7 @@ buildDatabase() {
                 echo "repo-add -s -n -R -k calrepo.db.tar.gz $pkg"
             else
                 repo-add -s -n -R -k "199980CE93F18E62" calrepo.db.tar.gz "$pkg"
-                rm -rf *.old*
+                rm -rf -- *.old*
             fi
         done < <(find -L "$PWD" -mindepth 1 -type f -regex '.*.pkg.tar.zst')
         cd .. || { echo "Couldn't cd into the parent folder." 1>&2 && exit 1; }
