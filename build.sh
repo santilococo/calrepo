6ce667173580f84f9223169149affde263b4f4c8
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-06 00:02:12 -0300

    Update build.sh and build db

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ buildDatabase() {
 
     while read -r pkg; do
         repo-add -s -n -R -k "199980CE93F18E62" calrepo.db.tar.gz "$pkg"
+	rm -rf *.old*
     done < "$1"
 
     cd "$lastFolder" || { echo "Couldn't cd into '$lastFolder'." 1>&2 && exit 1; }
