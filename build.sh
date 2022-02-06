aa3a2ab10706b9b7a63dd71d05f5a2a8b2febbe9
Santiago Lo Coco <santilococo.01@gmail.com>
2022-02-06 17:16:38 -0300

    Fix bugs

--- build.sh
+++ build.sh
@@ -3,19 +3,21 @@
 # TODO: Get the .pkg.tar.zst from calpkgs
 
 getAnyPackages() {
+    pkgs=($(find -H "$PWD/any" -mindepth 1 -type f -regex '.*.pkg.tar.zst.*'))
     for arch in "x86_64" "i686" "aarch64"; do
 	cd $arch
 
-	while read -r pkg; do
+	for pkg in "${pkgs[@]}"; do
 	    ln -sf "$pkg" "$(basename $pkg)"
-	done < "$1"
+	    #echo "ln -sf $pkg $(basename $pkg)"
+	done
 
 	cd ..
     done
 }
 
 buildDatabase() {
-    lastFolder=$(pwd -P)
+    #lastFolder=$(pwd -P)
     #cd db || { echo "Couldn't cd into 'db'." 1>&2 && exit 1; }
     for arch in "x86_64" "i686" "aarch64"; do
 	cd $arch
@@ -23,13 +25,16 @@ buildDatabase() {
 	while read -r pkg; do
 	    repo-add -s -n -R -k "199980CE93F18E62" calrepo.db.tar.gz "$pkg"
 	    rm -rf *.old*
-	done < <(find -H "$PWD" -mindepth 1 -type f -regex '.*.pkg.tar.zst')
+	    #echo "repo-add -s -n -R -k calrepo.db.tar.gz $pkg"
+	done < <(find -L "$PWD" -mindepth 1 -type f -regex '.*.pkg.tar.zst')
+
+	cd ..
     done
     #cd "$lastFolder" || { echo "Couldn't cd into '$lastFolder'." 1>&2 && exit 1; }
 }
 
 runScript() {
-    getAnyPackages <(find -H "$PWD/any" -mindepth 1 -type f -regex '.*.pkg.tar.zst.*')
+    getAnyPackages
     buildDatabase 
 }
 
